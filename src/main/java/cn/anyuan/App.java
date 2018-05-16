8a0fadeab5853df3ce645900b51c63cd1bd9f4ad
liangweiming <798651706@qq.com>
2018-05-16 14:42:02 +0800

    这是一git提交测试

---
+++ src/main/java/cn/anyuan/App.java
@@ -0,0 +1,13 @@
+package cn.anyuan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
